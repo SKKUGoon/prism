d7a3e74a2127b61215a635721e7cbcb26a7c8477
SKKUGoon <baesangil0906@gmail.com>
2025-02-20 02:16:17 +0900

    add commits

--- src/config.rs
+++ src/config.rs
@@ -1,5 +1,6 @@
 use std::env;
 
+#[allow(dead_code)] // There can be unused variable
 pub struct PrismEnvConfig {
     // Symbols
     pub symbol_binance_fut: String,
@@ -9,9 +10,9 @@ pub struct PrismEnvConfig {
     pub symbol_upbit_usdt: String,
 
     // Database Tables
-    pub table_fut: String,
+    pub table_fut: String, // For Raw data
     pub table_spt: String,
-
+    pub table_strat1: String, // For Strategy 1
     // Other
     pub data_dump: bool,
     pub channel_capacity: usize,
@@ -21,16 +22,18 @@ pub fn read_env_config() -> PrismEnvConfig {
     PrismEnvConfig {
         // Symbols
         symbol_binance_fut: env::var("SYMBOLS_BINANCE_FUT")
-            .unwrap_or_else(|_| "xrpusdt".to_string()),
+            .unwrap_or_else(|_| "NO_SYMBOL".to_string()),
         symbol_binance_spt: env::var("SYMBOLS_BINANCE_SPT")
-            .unwrap_or_else(|_| "xrpusdt".to_string()),
-        symbol_upbit_krw: env::var("SYMBOLS_UPBIT_KRW").unwrap_or_else(|_| "KRW-XRP".to_string()),
-        symbol_upbit_btc: env::var("SYMBOLS_UPBIT_BTC").unwrap_or_else(|_| "BTC-XRP".to_string()),
-        symbol_upbit_usdt: env::var("SYMBOLS_UPBIT_ETH").unwrap_or_else(|_| "USDT-XRP".to_string()),
+            .unwrap_or_else(|_| "NO_SYMBOL".to_string()),
+        symbol_upbit_krw: env::var("SYMBOLS_UPBIT_KRW").unwrap_or_else(|_| "NO_SYMBOL".to_string()),
+        symbol_upbit_btc: env::var("SYMBOLS_UPBIT_BTC").unwrap_or_else(|_| "NO_SYMBOL".to_string()),
+        symbol_upbit_usdt: env::var("SYMBOLS_UPBIT_USDT")
+            .unwrap_or_else(|_| "NO_SYMBOL".to_string()),
 
         // Database Tables
-        table_fut: env::var("TABLE_FUT").unwrap_or_else(|_| "feature_xrpusdt_future".to_string()),
-        table_spt: env::var("TABLE_SPT").unwrap_or_else(|_| "feature_xrpusdt_spot".to_string()),
+        table_fut: env::var("TABLE_FUT").unwrap_or_else(|_| "unspecified".to_string()),
+        table_spt: env::var("TABLE_SPT").unwrap_or_else(|_| "unspecified".to_string()),
+        table_strat1: env::var("STRATEGY1_TABLE").unwrap_or_else(|_| "unspecified".to_string()),
 
         // Other
         data_dump: env::var("DATA_DUMP").unwrap_or_else(|_| "false".to_string()) == "true",
