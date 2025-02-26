e54bfa5d3f6be90de619b55d2ed34f77cd4b221a
SKKUGoon <baesangil0906@gmail.com>
2025-02-26 14:08:29 +0900

    fix: remove unnecessary channels

--- src/channel.rs
+++ src/channel.rs
@@ -1,37 +1,21 @@
 use crate::data::{
-    liquidation::LiquidationData,
-    market::MarketData,
+    depth::OrderbookUpdateStream, liquidation::LiquidationData, market::MarketData,
     markprice::MarkPriceData,
-    orderbook::{OrderbookData, OrderbookUpdateStream},
 };
-use crate::prism::stream::FeatureProcessed;
 use tokio::sync::mpsc;
-
-pub struct OrderbookChannel {
-    // Orderbook -> Orderbook Processor
-    pub raw: (
-        mpsc::Sender<OrderbookUpdateStream>,
-        mpsc::Receiver<OrderbookUpdateStream>,
-    ),
-    // Orderbook Processor -> Engine
-    pub mng: (mpsc::Sender<OrderbookData>, mpsc::Receiver<OrderbookData>),
-}
 
 pub struct DataChannelPairs<Channel> {
     // Data Channel: Orderbook -> Engine
-    pub ob: OrderbookChannel,
+    pub ob: (
+        mpsc::Sender<OrderbookUpdateStream>,
+        mpsc::Receiver<OrderbookUpdateStream>,
+    ),
     // Data Channel: Aggtrade -> Engine
     pub agg: (mpsc::Sender<MarketData>, mpsc::Receiver<MarketData>),
-    // Data Channel: Mark Price -> Engine, Liquidation -> Engine
+    // Data Channel for futures(etc)
+    // Data Channel: Mark Price -> Engine,
+    // Data Channel: Liquidation -> Engine
     pub additional: Channel,
-}
-
-pub struct SystemChannelPairs {
-    // Engine Channel: Engine -> Executor
-    pub exec: (
-        mpsc::Sender<FeatureProcessed>,
-        mpsc::Receiver<FeatureProcessed>,
-    ),
 }
 
 pub struct Spot;
@@ -51,14 +35,10 @@ pub type FutureChannel = DataChannelPairs<Future>;
 impl SpotChannel {
     pub fn new(max_capacity: usize) -> Self {
         let (tx_ob_raw, rx_ob_raw) = mpsc::channel(max_capacity);
-        let (tx_ob_mng, rx_ob_mng) = mpsc::channel(max_capacity);
         let (tx_agg, rx_agg) = mpsc::channel(max_capacity);
 
         Self {
-            ob: OrderbookChannel {
-                raw: (tx_ob_raw, rx_ob_raw),
-                mng: (tx_ob_mng, rx_ob_mng),
-            },
+            ob: (tx_ob_raw, rx_ob_raw),
             agg: (tx_agg, rx_agg),
             additional: Spot,
         }
@@ -68,31 +48,17 @@ impl SpotChannel {
 impl FutureChannel {
     pub fn new(max_capacity: usize) -> Self {
         let (tx_ob_raw, rx_ob_raw) = mpsc::channel(max_capacity);
-        let (tx_ob_mng, rx_ob_mng) = mpsc::channel(max_capacity);
         let (tx_agg, rx_agg) = mpsc::channel(max_capacity);
         let (tx_mark, rx_mark) = mpsc::channel(max_capacity);
         let (tx_liq, rx_liq) = mpsc::channel(max_capacity);
 
         Self {
-            ob: OrderbookChannel {
-                raw: (tx_ob_raw, rx_ob_raw),
-                mng: (tx_ob_mng, rx_ob_mng),
-            },
+            ob: (tx_ob_raw, rx_ob_raw),
             agg: (tx_agg, rx_agg),
             additional: Future {
                 mark: (tx_mark, rx_mark),
                 liq: (tx_liq, rx_liq),
             },
-        }
-    }
-}
-
-impl SystemChannelPairs {
-    pub fn new(max_capacity: usize) -> Self {
-        let (tx_exec, rx_exec) = mpsc::channel(max_capacity);
-
-        Self {
-            exec: (tx_exec, rx_exec),
         }
     }
 }
